974a0453267b9532e842cf82619c789a7f2f05d8
Gareth van der Berg <gareth.shaun@gmail.com>
2015-02-20 09:19:05 +0200

    add reference to dispatcher interface

--- Plugins/Dialogs/XamCross.Plugins.Dialogs.WindowsPhoneStore/DialogService.cs
+++ Plugins/Dialogs/XamCross.Plugins.Dialogs.WindowsPhoneStore/DialogService.cs
@@ -1,14 +1,30 @@
 using System;
 
+using Cirrious.CrossCore;
+
+using XamCross.Plugins.Dispatcher;
+
 namespace XamCross.Plugins.Dialogs.WindowsPhoneStore
 {
     public class DialogService : AbstractDialogService
     {
         #region constructors
 
+        public DialogService()
+        {
+            _dispatcher = Mvx.Resolve<IDispatcherService>();
+
+            if (Guard.IsNull(_dispatcher))
+            {
+                throw new NullReferenceException("_dispatcher");
+            }
+        }
+
         #endregion
 
         #region fields
+
+        private readonly IDispatcherService _dispatcher;
 
         #endregion
 
