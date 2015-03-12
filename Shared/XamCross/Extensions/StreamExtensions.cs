2ba9ce3f0fbb8081f8b2318d5bea8ebc43c2de9e
Gareth van der Berg <gareth.shaun@gmail.com>
2015-03-12 08:37:46 +0200

    add async method

--- Shared/XamCross/Extensions/StreamExtensions.cs
+++ Shared/XamCross/Extensions/StreamExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace XamCross
 {
@@ -11,6 +12,25 @@ namespace XamCross
 
             var contents = new byte[length];
             var len = input.Read(contents, 0, contents.Length);
+
+            if (len == length)
+            {
+                return contents;
+            }
+
+            var rest = new byte[len];
+
+            Array.Copy(contents, rest, len);
+
+            return rest;
+        }
+
+        public static async Task<byte[]> ToBytesAsync(this Stream input, long offset, int length)
+        {
+            input.Seek(offset, SeekOrigin.Begin);
+
+            var contents = new byte[length];
+            var len = await input.ReadAsync(contents, 0, contents.Length);
 
             if (len == length)
             {
